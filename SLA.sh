74ca23823464e332470f51b8ff417aff490e22ce
Antonio Prado <antonio@prado.it>
2018-09-22 11:29:39 +0200

    Update SLA.sh

--- SLA.sh
+++ SLA.sh
@@ -88,7 +88,7 @@ echo Trying to reach google.com
 echo ##################################
 ((count = 3))
 while [[ $count -ne 0 ]] ; do
-    ping6 -qc 1 google.com
+    ping6 -qc 1 2001:1ac0:0:200:0:a5d1:6004:27
     rc=$?
     if [[ $rc -eq 0 ]] ; then
         ((count = 1))
